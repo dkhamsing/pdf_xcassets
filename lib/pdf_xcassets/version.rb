83a168c72426f0ce4e8f901673f57db71aea5cd6
dkhamsing <dkhamsing@cirruspath.com>
2015-12-24 07:10:37 -0800

    project url

--- lib/pdf_xcassets/version.rb
+++ lib/pdf_xcassets/version.rb
@@ -2,7 +2,7 @@
 module PdfXcassets
   PROJECT_NAME = 'pdf_xcassets'
   PROJECT_DESCRIPTION = 'Generate Xcode xcasset from pdf assets.'
-  PROJECT_URL = 'https://github.com/dkhamsing'
+  PROJECT_URL = 'https://github.com/dkhamsing/pdf_xcassets'
 
   VERSION = "0.1.0"
 end
