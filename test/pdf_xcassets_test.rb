8b0a74a756a2d9d55f65b1ab27744653960f99dd
dkhamsing <dkhamsing@cirruspath.com>
2016-03-18 10:19:19 -0700

    [test] check json key

--- test/pdf_xcassets_test.rb
+++ test/pdf_xcassets_test.rb
@@ -1,14 +1,21 @@
-# require 'JSON'
+require 'json'
+require 'pp'
 
 ASSET = 'resources'
 PATH = 'test/fixtures'
-
-# puts "testing pdf_xcassets with name: #{ASSET} and path: #{PATH}"
-# `pdf_xcassets #{ASSET} #{PATH}`
+CONTENTS = 'Contents.json'
 
 xcassets = "#{ASSET}.xcassets"
-CONTENTS = 'Contents.json'
 path = "#{xcassets}/#{CONTENTS}"
 puts "Checking for #{path}"
 exit 1 unless File.exist? path
 puts "#{CONTENTS} is created ✅"
+
+print "Checking #{CONTENTS} format "
+c = JSON.load File.new(path)
+puts '✅'
+
+INFO = 'info'
+puts "Missing #{INFO}" unless c.key? INFO
+
+puts "Keys are present in #{CONTENTS} ✅"
