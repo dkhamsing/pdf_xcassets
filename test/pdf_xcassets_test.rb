811bd5df44324371ed45263f5640129759533ecd
dkhamsing <dkhamsing@cirruspath.com>
2016-03-18 10:34:40 -0700

    [test] more tests

--- test/pdf_xcassets_test.rb
+++ test/pdf_xcassets_test.rb
@@ -1,9 +1,22 @@
+require 'pdf_xcassets'
 require 'json'
-require 'pp'
+# require 'pp'
 
 ASSET = 'resources'
 PATH = 'test/fixtures'
+
 CONTENTS = 'Contents.json'
+INFO = 'info'
+VERSION = 'version'
+AUTHOR = 'author'
+
+def check_key(hash, key)
+
+  unless hash.key? key
+    puts "Missing #{key} 🔴"
+    exit 1
+  end
+end
 
 xcassets = "#{ASSET}.xcassets"
 path = "#{xcassets}/#{CONTENTS}"
@@ -15,7 +28,19 @@ print "Checking #{CONTENTS} format "
 c = JSON.load File.new(path)
 puts '✅'
 
-INFO = 'info'
-puts "Missing #{INFO}" unless c.key? INFO
-
+check_key c, INFO
+check_key c[INFO], VERSION
+check_key c[INFO], AUTHOR
 puts "Keys are present in #{CONTENTS} ✅"
+
+print 'Checking number of assets '
+pdf = PdfXcassets::get_pdf_assets PATH + '/'
+expect = 3
+exit 1 unless expect == pdf.count
+puts '✅'
+
+glob = Dir.glob "#{xcassets}/**/*"
+imagesets = glob.select { |x| x.downcase.include? 'imageset' }
+expect = 9
+exit 1 unless expect == imagesets.count
+puts 'Correct number of imagesets ✅'
